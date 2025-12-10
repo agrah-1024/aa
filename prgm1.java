b5d2260dd42672b6642f111aa49e55ee516aa16a
agrah-1024 <aagrahmk@gmail.com>
2025-12-10 22:48:56 +0530

    Add files via upload

---
+++ prgm1.java
@@ -0,0 +1,15 @@
+public class prgm1 {
+ public static void main(String[] args){
+ int a,b,c,s,m,d;
+ a=10;
+ b=30;
+ c=a+b;
+ s=a-b;
+ m=a*b;
+ d=a/b;
+System.out.println("sum="+c);
+System.out.println("substraton="+s);
+System.out.println("multiple="+m);
+System.out.println("divison="+d);
+}
+}
